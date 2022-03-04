1fbf98a4636a8983e5c415148d0ee3c773f9f4ac
Hugo <hugo.carrier@umontreal.com>
2022-03-04 12:52:46 -0500

    Modification du test pour aller avec le probleme 2

--- test/test.py
+++
@@ -1,6 +0,0 @@
-
-import requests
-
-url = "http://127.0.0.1:5000/logic"
-
-print( requests.request("GET", url, headers={}, params={}).json())
---
+++ test/test_q2.py
@@ -0,0 +1,9 @@
+#Par Carrier Hugo 
+#tester l'api
+import requests
+
+url = "http://127.0.0.1:8000/logic"
+assert requests.request("GET", url, headers={}, params={}).json() == {'notionalRetrival': {'formula': 'Quantity * last close price', 'source': 'Yahoo Finance'}}
+
+url = "http://127.0.0.1:8000/logic?logic=notionalRetrival"
+assert requests.request("GET", url, headers={}, params={}).json() == {'formula': 'Quantity * last close price', 'source': 'Yahoo Finance'}
