8117810804dce8a44a42b97a5be80a4d2f1481a7
Hugo <hugo.carrier@umontreal.com>
2022-03-04 12:53:21 -0500

    Terminer question 2: un api qui roule en local.

--- SG_Yahoo_q2/SG_Yahoo_q2.py
+++ SG_Yahoo_q2/SG_Yahoo_q2.py
@@ -1,36 +1,48 @@
+#Par Carrier Hugo 
 import json
-from flask import Flask, jsonify
-from flask_restful import Resource, Api, reqparse
+from flask import Flask, request
 from pathlib import Path
 
 
-class Logic():
-    def get():
+class Logic:
+    #Class to work with logic information
+    #Could add put, post, delete in the futur. 
+    def get(self,logic_to_get = None):
+        """
+        get , get infomation from a json file 
+        :param logic_to_get:  logic to get, if none will get all the logic in the file
+        :return: logic in the form of json, return empty if file not found
+        """
         # could be an acces to a data base in real life
         base_path = str(Path(__file__).parent.resolve())
-        full_path = base_path + "/logic.json"
-        file = open(full_path, "r")
+        full_path = base_path + '/logic.json'
+        file = open(full_path, 'r')
         # get data from open file or return error
         try:
             all_data = json.load(file)
             file.close
-            return all_data
+            if( logic_to_get is None):
+                return all_data
+            else :
+                return all_data[logic_to_get]
         except:
             file.close
-            return "no logic found"
+            return {}
 
-
+logic = Logic()
 app = Flask(__name__)
 
-
+#front page
 @app.route('/', methods=['GET'])
 def home():
-    return '''<h1>Un beau probleme</h1>
+    return '''<h1>Un beau problème</h1>
 <p>Un Seul Api existe : Logic</p>'''
 
 
 @app.route('/logic', methods=['GET'])
 def get_logic():
-    return Logic.get()
+    logic_to_get = request.args.get('logic')
+    return logic.get(logic_to_get)
 
-app.run()
+
+app.run(None,port = 8000)
